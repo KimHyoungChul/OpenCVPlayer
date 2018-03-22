609d8f5267c6abd73e12dcac264a4f2fd6acd48b
renhui <1418757529@qq.com>
2018-03-22 15:23:02 +0800

    基本播放功能完成

---
+++ app/src/main/java/com/renhui/opencvplayer/demo/GlobalConfig.java
@@ -0,0 +1,19 @@
+package com.renhui.opencvplayer.demo;
+
+/**
+ * 全局配置 -- 目前主要是存放一些常量
+ * Created by renhui on 2017/8/7.
+ */
+public class GlobalConfig {
+
+    // 目前对rtmp输入的flv格式的数据支持的比较好，其他格式目前看，不是很好，需要做兼容
+
+    public static final String INPUT_VIDEO_PATH = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
+
+    // public static final String INPUT_VIDEO_PATH = "http://live.hkstv.hk.lxdns.com/live/hks/playlist.m3u8";
+
+    // public static final String INPUT_VIDEO_PATH = "rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov";
+
+    // 输出的流地址  目前仅支持rtmp
+    public static final String OUTPUT_STREAM_PATH = "rtmp://push-ws1.csslcloud.net/src/40F645371FE5559E9C33DC5901307461?token=jXbhgsOR";
+}
--- app/src/main/java/com/renhui/opencvplayer/demo/MainActivity.java
+++ app/src/main/java/com/renhui/opencvplayer/demo/MainActivity.java
@@ -1,13 +1,57 @@
 package com.renhui.opencvplayer.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.renhui.playerlib.VideoSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoSurfaceView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // 保持屏幕常亮
         setContentView(R.layout.activity_main);
+        surfaceView = (VideoSurfaceView) findViewById(R.id.surface);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+        } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        surfaceView.initPlayer(GlobalConfig.INPUT_VIDEO_PATH);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        surfaceView.releasePlaer();
     }
 }
